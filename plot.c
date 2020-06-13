3bf93126c03eaf5b2c891e9879d2199e90ae1b6f
Ben Moyer <ben.s.moyer@gmail.com>
2020-06-13 12:43:37 -0700

    basic plotting works, no floating-point slices yet

--- plot.c
+++ plot.c
@@ -4,8 +4,13 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
+#include <math.h>
 
 #include "array.h"
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 #define BATCH_SIZE 1000000
 
@@ -20,19 +25,66 @@ WINDOW* mainwin = NULL;
 
 // Normalizes data with respect to x and y
 void summarize_bargraph(array* samples, int* result, int y, int x) {
-    result = malloc(x * sizeof(int));
-    for(int i = 0; i < samples->size; i++) {
+    /*
+    for(int i = 0; (i < samples->size) && (i < x); i++) {
+        result[i] = samples->array[i]->val;
     }
+    */
+
+    int slice_size = MAX(samples->size / x, 1);
+    int cur_slice = 0;
+    for(int i = 0; i < samples->size && cur_slice < x; i+=slice_size) {
+        int avg = 0;
+        for(int j = i; j < (i+slice_size); j++) {
+            avg += samples->array[j]->val;
+        }
+        avg /= slice_size;
+        result[cur_slice++] = avg;
+    }
+/*
+    double slice_size = (double) (samples->size) / (double) x;
+    //printf("slice size: %f, samples->size: %d -- %d\n", slice_size, samples->size, 0 < (double) (samples->size) );
+    for(double i = 0; i < (double) (samples->size); i+=slice_size) {
+        double start = i;
+        double end = start + slice_size;
+
+       // double start_int, start_frac;
+        //start_frac = modf(start, &start_int);
+        double start_frac = ceil(start) - start;
+        double end_frac = 
+    }
+    //printf("RETURN\n");
+    */
 }
 
 void paint_bargraph(array* samples) {
     int num_rows, num_cols;
     getmaxyx(mainwin, num_rows, num_cols);
 
+    int* result = malloc(num_cols*sizeof(int));
+    memset(result, 0, num_cols*sizeof(int));
+    summarize_bargraph(samples, result, num_rows, num_cols);
+    for(int i = 0; i < 40; i++) {
+        //result[i] = i;
+        //result[i+40] = 40 - i;
+    }
+    for(int i = 0; i < num_cols; i++)  {
+        int height = result[i];
+        //printf("(x, y): (%d, %d)\n", i, height);
+        for(int j = 0; j < height; j++) {
+            mvwaddch(mainwin, num_rows - j - 2, i + 1, ACS_VLINE);
+            //mvwaddch(mainwin, num_rows - j - 2, 80, ACS_VLINE);
+        }
+        //int i = result[0];
+        //mvwaddch(mainwin, 10, 10, ACS_VLINE);
+    }
+    
+    //mvwprintw(mainwin, 20, 10, "%s", "THIS IS A TEST");
+
     for(int i = 0; i < num_cols; i++) {
         if(i < samples->size) {
             for(int j = 0; j < samples->array[i]->val; j++) {
-                mvwaddch(mainwin, num_rows - j - 2, i + 1, ACS_VLINE);
+                //mvwaddch(mainwin, num_rows - j - 2, i + 1, ACS_VLINE);
             }
         }
     }
@@ -55,7 +107,8 @@ void paint(array* samples) {
 
     paint_bargraph(samples);
 
-    mvprintw(0, 1, " Data size: %d ", samples->size);
+    time_t ltime = time(NULL);
+    mvprintw(0, 1, " Data size: %d - %s", samples->size, asctime(localtime(&ltime)));
     refresh();
 }
 
